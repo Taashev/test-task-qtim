05435c61c26aca4f207e26c5a099bcdb90cb21af
Taashev <taashev.a.r92@yandex.ru>
2024-03-22 17:54:08 +0300

    DEV-5: Настройка проекта

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { configSettings } from './configs/config-settings';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(configSettings)],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  const NODE_ENV = configService.get('NODE_ENV');
+  const HOST = configService.get('HOST');
+  const PORT = configService.get('PORT');
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+  await app.listen(PORT, HOST, () => {
+    if (NODE_ENV !== 'production') {
+      console.log('server is running in development mode');
+      console.log('PORT: ', PORT);
+      console.log('PID: ', process.pid);
+    }
+  });
 }
+
 bootstrap();
